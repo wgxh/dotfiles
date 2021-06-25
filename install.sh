3aa4d6305856b62d7eff2adaebe03e2275e7301a
wgxh-cli <15184439535@qq.com>
2021-06-25 18:22:30 +0800

    add .gitconfig, .xinitrc and more.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-ln -s ~/dotfiles/.bashrc ~/.bashrc
+echo "==> Start to install..."
+
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
+ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sf ~/dotfiles/picom.conf ~/.config/picom.conf
 cd dwm
 sudo make clean install
 cd ../st
 sudo make clean install
+
+echo "==> Done!"
