5b25f2c23e383d56b5b9fd137f2cae15ba8f540c
wgxh-cli <15184439535@qq.com>
2021-06-26 12:58:27 +0800

    add some config files.

---
+++ .dwm/autostart.sh
@@ -0,0 +1,3 @@
+fcitx &
+picom &
+feh --bg-scale ~/Pictures/9c1dc7851eb22b2ff21eef15b3bc413b9b49a789_00.gif
--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
 ln -sf ~/dotfiles/picom.conf ~/.config/picom.conf
+ln -sf ~/dotfiles/bspwm ~/.config/bspwm
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.dwm ~/.dwm
+sudo ln -sf ~/dotfiles/pacman.conf /etc/pacman.conf
 cd dwm
 sudo make clean install
 cd ../st
